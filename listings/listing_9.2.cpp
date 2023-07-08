a12f8ac4c83abdc1472b42e86c18e545026f4ec6
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-07-08 15:37:53 +0100

    `std::result_of` -> `std::invoke_result_t`
    - `std::result_of` was deprecated as of C++17, and removed in C++20
    - opt for `std::invoke_result_t` in its place for all instances
    - also improves brevity

--- listings/listing_9.2.cpp
+++ listings/listing_9.2.cpp
@@ -48,10 +48,10 @@ public:
     std::deque<function_wrapper> work_queue;
 
     template<typename FunctionType>
-    std::future<typename std::result_of<FunctionType()>::type>
+    std::future<std::invoke_result_t<FunctionType&&>>
     submit(FunctionType f)
     {
-        typedef typename std::result_of<FunctionType()>::type result_type;
+        typedef std::invoke_result_t<FunctionType&&> result_type;
         
         std::packaged_task<result_type()> task(std::move(f));
         std::future<result_type> res(task.get_future());
