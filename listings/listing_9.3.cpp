e78bb109fea51810982969596829d7f9e402b8c5
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-07-28 08:20:54 +0100

    correct lambda function
    - accumulate_block::operator() returns type _Tp
    - we must call `return` on the struct's call inside the lambda to capture this return type
    - without this, the code will fail to compile, and provide the following error:
    - - "No viable overloaded '='"

--- listings/listing_9.3.cpp
+++ listings/listing_9.3.cpp
@@ -30,7 +30,7 @@ T parallel_accumulate(Iterator first,Iterator last,T init)
         Iterator block_end=block_start;
         std::advance(block_end,block_size);
         futures[i]=pool.submit([=] {
-            accumulate_block<Iterator,T>()(block_start,block_end);
+            return accumulate_block<Iterator,T>()(block_start,block_end);
         });
         block_start=block_end;
     }
